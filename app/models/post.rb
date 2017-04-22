3bffbe7a67cdbafa2b0cf1f28036a4f2a1359c3b
aleks2414 <alejandroromo14@gmail.com>
2017-04-21 22:24:21 -0500

    description

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,10 @@ class Post < ActiveRecord::Base
     is_impressionable
     acts_as_votable
 
+  validates :url, presence: true, :format => URI::regexp(%w(http https))
+
+  before_save :scrape_with_grabbit
+
   mount_uploader :image, ImageUploader # Tells rails to use this uploader for this model.
 
     
@@ -15,12 +19,28 @@ class Post < ActiveRecord::Base
   self.calificacion = self.amazon + self.calidad + self.garantia + self.precio + self.precio
   end
 
-    paginates_per 12
+    paginates_per 30
 
     extend FriendlyId
     friendly_id :title, use: [:finders, :slugged]
 
     # mount_uploader :photo, PhotoUploader
+
+    private 
+
+    def scrape_with_grabbit
+
+        # I highly recommend passing the following call off to a Resque worker, or Delayed Job queue.
+        # The reason is that Grabbit will attempt to access the remote URL. If there is a network problem,
+        # or the remote URL is unavailable, the following line could hang up your Rails process.
+
+        data = Grabbit.url(url)
+
+        if data
+            self.description = data.description
+        end
+
+    end
 
 
 end
